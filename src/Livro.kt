0108919b9610b3080edf7c9b092466ac8035485a
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-06-02 17:30:50 -0400

    feat: implementa a classe Livro com propriedades e método de verificação de disponibilidade

--- src/Livro.kt
+++ src/Livro.kt
@@ -1,4 +1,21 @@
 package src
 
-class Livro {
+class Livro(
+    val id: String,
+    val titulo: String,
+    val isbn: String,
+    val autor: Autor,
+    val disponivel: Boolean
+) {
+
+    fun verificaDisponibilidades(): Boolean{
+        if(this.disponivel){
+            println("O livro ${this.titulo} está disponível.")
+            return true
+        }
+        else{
+            println("O livro ${this.titulo} não está disponível.")
+            return false
+        }
+    }
 }
