4ae92ac680f42bd3d22cab55825a4fb2081b6105
Arthur.Gomes <arthurlacerdagomes123@gmail.com>
2025-06-03 09:41:08 -0400

    feat: Adicionando classe emprestimo e funcionalidade atualizarStatus

--- src/Emprestimo.kt
+++ src/Emprestimo.kt
@@ -1,4 +1,32 @@
 package src
 
-class Emprestimo {
+import java.util.UUID
+
+class Emprestimo (
+    val livro: Livro = Livro(),
+    val usuario: Usuario = Usuario(),
+    val dataEmprestimo: String,
+    var dataDevolucao: String?,
+    var status: String
+
+){
+    companion object {
+        const val STATUS_EM_USO = "Em uso"
+        const val STATUS_DISPONIVEL = "Disponível"
+    }
+    val id: String = UUID.randomUUID().toString()
+
+    public fun atualizarStatus(status: String){
+        if(status == STATUS_EM_USO || status == STATUS_DISPONIVEL){
+            if(status == "Em uso"){
+                this.status = STATUS_EM_USO
+                println("Status atualizado com sucesso! ${this.status}")
+            }else{
+                this.status = STATUS_DISPONIVEL
+                println("Status atualizado com sucesso! ${this.status}")            }
+        }else{
+            println("Valor ${status} é inválido por favor digite Em uso ou disponível ")
+        }
+
+    }
 }
